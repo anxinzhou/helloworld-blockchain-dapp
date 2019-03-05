4171e1062029b8ec3af34a699d737bff3236b6df
anxin <anxin@anxins-MacBook-Pro.local>
2019-03-05 18:17:21 +0800

    https server

--- back-end/main.js
+++ back-end/main.js
@@ -6,7 +6,13 @@ const cors = require('koa2-cors');
 const bodyParser = require('koa-bodyparser');
 const Web3 = require('web3');
 const config = require('./config.json');
+const fs = require('fs');
+const https = require('https');
 
+const options = {
+	key: fs.readFileSync('../pem/key.pem'),
+	cert: fs.readFileSync('../pem/cert.pem')
+};
 const web3 = new Web3(config.httpPort);
 const contract = new web3.eth.Contract(config.abi, config.address);
 const account = web3.eth.accounts.privateKeyToAccount('0x65CF458E20E12991BA6AEBC210B8293AD5B3495F585CECC6D22DE77933DE2B0F');
@@ -19,7 +25,7 @@ console.log("app start");
 app.use(cors());
 app.use(bodyParser());
 app.use(router.routes());
-app.listen(4000, '0.0.0.0');
+https.createServer(options,app.callback()).listen(4000);
 
 
 async function sendTransaction(funcName, ...args) {
